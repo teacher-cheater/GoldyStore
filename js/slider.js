113177a6d4263bbcec16b8cff98d89dc0b06112f
teacher_cheater <johnnykayfun@gmail.com>
2023-02-01 01:39:18 +0300

    add section sales

--- js/slider.js
+++ js/slider.js
@@ -27,3 +27,41 @@ function showSlides() {
 }
 
 showSlides();
+
+//----slider-swiper
+new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+    pageUpDown: true,
+  },
+  autoHeight: true,
+  watchOverfloy: true,
+  autoplay: {
+    delay: 3000,
+  },
+  speed: 600,
+  effect: 'fade',
+  breakpoints: {
+    320: {
+      slidePerView: 1,
+    },
+    480: {
+      slidePerView: 1,
+    },
+    992: {
+      slidePerView: 1,
+    },
+  },
+  preloadImages: false,
+  lazy: {
+    loadOnTransitionStart: false,
+    loadPrevNext: false,
+  },
+  watchSlidesProgress: true,
+  watchSlidesVisibility: true,
+})
