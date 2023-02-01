34dd8e85237047a02d92c9d18975c7c05c51848b
teacher_cheater <johnnykayfun@gmail.com>
2023-02-01 23:52:52 +0300

    add card sales

--- js/slider.js
+++ js/slider.js
@@ -1,67 +1,71 @@
 
-const btnPrev = document.querySelector('.slider__btn-prev')
-const btnNext = document.querySelector('.slider__btn-next')
+//const btnPrev = document.querySelector('.slider__btn-prev')
+//const btnNext = document.querySelector('.slider__btn-next')
 
-//btnPrev.addEventListener('click', ()=> plusSlides(-1))
+////btnPrev.addEventListener('click', ()=> plusSlides(-1))
 
 
-let slideIndex = 0;
+////let slideIndex = 0;
 
-function showSlides() {
-  let i;
-  const slides = document.getElementsByClassName("my-slides");
+////function showSlides() {
+////  let i;
+////  const slides = document.getElementsByClassName("my-slides");
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+////  for (i = 0; i < slides.length; i++) {
+////    slides[i].style.display = "none";
+////  }
 
-  slideIndex++;
+////  slideIndex++;
 
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
+////  if (slideIndex > slides.length) {
+////    slideIndex = 1;
+////  }
 
-  slides[slideIndex - 1].style.display = "flex";
+////  slides[slideIndex - 1].style.display = "flex";
 
-  setTimeout(showSlides, 555000); // Change image every 2 seconds
-}
+////  setTimeout(showSlides, 555000); // Change image every 2 seconds
+////}
 
-showSlides();
+////showSlides();
 
-//----slider-swiper
-new Swiper('.swiper', {
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  grabCursor: true,
-  keyboard: {
-    enabled: true,
-    pageUpDown: true,
-  },
-  autoHeight: true,
-  watchOverfloy: true,
-  autoplay: {
-    delay: 3000,
-  },
-  speed: 600,
-  effect: 'fade',
-  breakpoints: {
-    320: {
-      slidePerView: 1,
-    },
-    480: {
-      slidePerView: 1,
-    },
-    992: {
-      slidePerView: 1,
-    },
-  },
-  preloadImages: false,
-  lazy: {
-    loadOnTransitionStart: false,
-    loadPrevNext: false,
-  },
-  watchSlidesProgress: true,
-  watchSlidesVisibility: true,
-})
+////----slider-swiper
+//new Swiper('.swiper', {
+//  navigation: {
+//    nextEl: "swiper-button-next",
+//    prevEl: "swiper-button-prev",
+//  },
+//  pagination: {
+//    el: '.swiper-pagination',
+//    clickable: true,
+//  },
+//  grabCursor: true,
+//  keyboard: {
+//    enabled: true,
+//    pageUpDown: true,
+//  },
+//  autoHeight: true,
+//  watchOverfloy: true,
+//  autoplay: {
+//    delay: 3000,
+//  },
+//  speed: 600,
+//  effect: 'fade',
+//  breakpoints: {
+//    320: {
+//      slidePerView: 1,
+//    },
+//    480: {
+//      slidePerView: 1,
+//    },
+//    992: {
+//      slidePerView: 1,
+//    },
+//  },
+//  preloadImages: false,
+//  lazy: {
+//    loadOnTransitionStart: false,
+//    loadPrevNext: false,
+//  },
+//  watchSlidesProgress: true,
+//  watchSlidesVisibility: true,
+//})
